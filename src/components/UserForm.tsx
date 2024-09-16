c46f977c054e27b81f2dc798b26bfc81748caa07
Timur <timur.mosenkov@gmail.com>
2024-09-16 16:10:45 +0300

    add user form

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,9 +1,33 @@
-
+import classes from "./userForm.module.scss";
 
 const UserForm = () => {
   return (
-    <div>UserForm</div>
-  )
-}
+    <form className={classes.form}>
+      <div className={classes['form-fields']}>
+        <div>
+          <label htmlFor="">First Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Last Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Age</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Password</label>
+          <input type="text" />
+        </div>
+        <button className={classes['create-btn']}>Create Account</button>
+      </div>
+    </form>
+  );
+};
 
-export default UserForm
+export default UserForm;
