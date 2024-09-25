0525009413eea6eb85af26f6ba511261eb6141df
Timur <timur.mosenkov@gmail.com>
2024-09-25 19:06:06 +0300

    add login submit function

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,28 @@
+import { FormEvent, useState } from "react";
 import classes from "./login.module.scss";
 
+
+
 const Login = () => {
+  const [email, setEmail] = useState<string>()
+  const [password, setPassword] = useState<string>()
+
+  const onSubmitForm = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={onSubmitForm}>
       <div className={classes["form-fields"]}>
         <div className={classes["login-inp"]}>
-          <label htmlFor="">Email</label>
-          <input type="text" />
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email"  onChange={(e) => setEmail(e.target.value)}/>
         </div>
         <div className={classes["login-inp"]}>
-          <label htmlFor="">Password</label>
-          <input type="password" />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} />
         </div>
-
         <div className={classes["login-btn"]}>
           <button>Login</button>
         </div>
