c253daab8ac71859fcde982f9d5fa2d9f2eeb45f
Timur <timur.mosenkov@gmail.com>
2024-10-02 21:57:09 +0300

    make own tasks accessable only fro logged user

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
         />
         <Route path="signup" element={<Signup />} />
         <Route path="login" element={<Login />} />
-        <Route path="tasks" element={<Tasks />} />
+        {user.length !== 0 && <Route path="tasks" element={<Tasks />} />}
       </Routes>
     </div>
   );
