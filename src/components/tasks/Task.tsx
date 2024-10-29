d52bb131cc6a5c0f54598f3e128c4444ce5843fa
Timur <timur.mosenkov@gmail.com>
2024-10-29 11:36:50 +0200

    make task editable

--- src/components/tasks/Task.tsx
+++ src/components/tasks/Task.tsx
@@ -1,26 +1,43 @@
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import classes from './task.module.scss'
+import classes from "./task.module.scss";
+import { useState } from "react";
 
 interface RootState {
   users: User[] | null;
 }
 
 const Task = () => {
-  const { id } = useParams();
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const { id } = useParams<{ id: string }>();
   const task = useSelector((state: RootState) => {
-    return state.users.tasks.find(task => task.id === id)
+    return state.users.tasks.find((task) => task.id === id);
   });
 
-
+  if (!task) {
+    return <p>Task not found</p>;
+  }
 
   return (
     <div className={classes.task}>
-      <h2>{task.title}</h2>
-      <p>{task.description}</p>
-      <div className={classes['btn-cnt']}>
-        <button>complete</button>
-        <button>edit</button>
+      {!isEditing ? (
+        <>
+          <h2>{task.title}</h2>
+          <p>{task.description}</p>
+        </>
+      ) : (
+        <>
+          <input type="text" defaultValue={task.title} />
+          <input type="text" defaultValue={task.description} />
+        </>
+      )}
+      <div className={classes["btn-cnt"]}>
+        <button>Complete</button>
+        <>
+         {!isEditing ? <button onClick={() => setIsEditing(true)}>Edit</button> : 
+          <button onClick={() => setIsEditing(false)}>save</button>
+         }
+        </>
       </div>
     </div>
   );
