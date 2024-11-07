ec79650b5333318fdcf980b9a0374c86cfe07b78
Timur <timur.mosenkov@gmail.com>
2024-11-07 20:55:29 +0200

    add lowercase function to email field

--- src/reduers/userReducer.tsx
+++ src/reduers/userReducer.tsx
@@ -11,7 +11,6 @@ export type User = {
 
 type UserState = User | null;
 
-// Set the initial state to null
 const initialState: UserState = null;
 
 
@@ -24,7 +23,7 @@ const userSlice = createSlice({
     },
 
     unsetUser(state, action: PayloadAction<null>) {
-      return action.payload;
+      return action.payload
     },
   },
 });
